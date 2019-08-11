47c618049e2bf61897d630aeadd8da00c6ba9742
Nanna <nanna@decorraid.com>
2019-08-11 16:15:27 +0200

    first commit

--- lifebymunkholm/karma.conf.js
+++ lifebymunkholm/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/GuidelinesByMunkholm'),
+      dir: require('path').join(__dirname, './coverage/lifebymunkholm'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- lifebymunkholm/src/app/app.component.spec.ts
+++ lifebymunkholm/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'GuidelinesByMunkholm'`, () => {
+  it(`should have as title 'lifebymunkholm'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('GuidelinesByMunkholm');
+    expect(app.title).toEqual('lifebymunkholm');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to GuidelinesByMunkholm!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lifebymunkholm!');
   });
 });
